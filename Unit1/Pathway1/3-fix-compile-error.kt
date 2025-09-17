5d7652592c7bed447ce373d454232d021bcb082d
Tran Dang Duat <23020023@vnu.edu.vn>
2025-09-17 08:26:51 +0700

    u1 -> p1 -> problem 2-4

---
+++ Unit1/Pathway1/3-fix-compile-error.kt
@@ -0,0 +1,4 @@
+// The string must end with " instead of '. Also the function argument must close with ) instead of }
+fun main() {
+    println("New chat message from a friend")
+}
---
+++ Unit1/Pathway1/4-string-templates.kt
@@ -0,0 +1,10 @@
+// The problem is the variable created using "val" cannot be reassigned, must use "var" instead
+fun main() {
+    var discountPercentage: Int = 0
+    var offer: String = ""
+    val item = "Google Chromecast"
+    discountPercentage = 20
+    offer = "Sale - Up to $discountPercentage% discount on $item! Hurry up!"
+
+    println(offer)
+}
